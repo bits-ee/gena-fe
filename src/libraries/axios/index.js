6a4c61cf44d84efc8cfa2cb88fe9d074460585e9
Zahhar Kirillov <zahhar@gmail.com>
2021-06-26 20:28:02 +0200

    dotenv for axios config

--- src/libraries/axios/index.js
+++ src/libraries/axios/index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 window.axios = axios
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = process.env.VUE_APP_DATA_SERVICE_URL;
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 let sessionKey = sessionStorage.getItem('sessionKey');
 if(sessionKey) axios.defaults.headers.common.Authorization = 'Bearer '+sessionKey
