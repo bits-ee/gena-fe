22a65e4b4f2c1f7e33c87820b6ca4149498d3e26
Zahhar Kirillov <zahhar@gmail.com>
2021-08-10 00:27:29 +0200

    TomSelect fixed with minor cleanup. App name added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './libraries/router'
-import store from './libraries/store'
-const _ = require('lodash')
-import './libraries/axios'
 import i18n from './libraries/i18n'
+import store from './libraries/store'
+import router from './libraries/router'
+import _ from 'lodash';
 import langDetector from './libraries/i18n/langDetector'
-import './assets/css/style.css';
+import './libraries/axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/style.css'
 
 createApp(App).use(i18n).use(store).use(router).use(_).use(langDetector).mount('#app')
