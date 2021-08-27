4e06a9325474bf3770980aacdb0ffd2f99b929fd
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-08-27 06:45:30 -0700

    geolocation find

---
+++ src/components/geoLocAutoDetect.vue
@@ -0,0 +1,22 @@
+<template id="geoLocAutoDetect-component">
+
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+ name: "geoLocAutoDetect",
+    mounted() {
+      this.$getLocation(options)
+          .then(coordinates => {
+            alert(coordinates);
+          });
+    }
+  }
+)
+</script>
+
+<style scoped>
+
+</style>
