e3c617f42715c80b9f21684b84e44b7a40526348
Aleksandr Ostromogilskii <60792658+Shapus@users.noreply.github.com>
2021-09-20 17:29:07 +0300

    app component specification as comment

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,2 +1,9 @@
 import app from '@/App.vue'
 
+// MOUNT
+//1. if sessionStorage['sessionKey'] is exist, call 'FETCH_AVATAR' action
+    //1.1. if success, call 'login' mutation with sessionStorage['sessionKey'] as payload
+    //1.2. else call 'logout' mutation
+//2. call 'FETCH_CONFIG' action
+    //2.1. if success, set 'init' to true and load a page
+    //2.2. else show error
