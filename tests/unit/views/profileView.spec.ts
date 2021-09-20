fc10a4620627a4a760442c02642f496acb8f42db
Aleksandr Ostromogilskii <60792658+Shapus@users.noreply.github.com>
2021-09-20 17:23:11 +0300

    views specifications as comments

--- tests/unit/views/profileView.spec.ts
+++ tests/unit/views/profileView.spec.ts
@@ -1,2 +1,13 @@
 import profile from '@/views/profile.vue'
 
+// VUEX DATA
+// boolean 'is_auth'
+
+// MOUNT
+//1. if 'is_auth' is true call actions 'FETCH_LOCATIONS',
+//              'FETCH_AVATAR',
+//              'FETCH_DETAILS',
+//              'FETCH_CHANNELS',
+//              'FETCH_USER_LOCATIONS',
+// actions. If all was success, set 'init' param to true and load components
+//2. else redirect to signup
--- tests/unit/views/signupView.spec.ts
+++ tests/unit/views/signupView.spec.ts
@@ -1,2 +1,8 @@
 import signup from '@/views/signup.vue'
 
+// VUEX DATA
+//boolean 'is_auth'
+
+// MOUNT
+//1. if 'is_auth' is false, show 'emailLogin' and 'tgLogin' components
+//2. else show 'go to profile' button redirecting to 'profile' on click 
